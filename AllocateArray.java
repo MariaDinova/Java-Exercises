d1d02d32b5bd5518b6e901395acfcfb7e9bfc885
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:16:22 +0300

    Update AllocateArray.java

--- AllocateArray.java
+++ AllocateArray.java
@@ -1,3 +1,6 @@
+/*Write a program that allocates array of N integers, initializes each element by its 
+index multiplied by 5 and the prints the obtained array on the console.*/
+
 import java.util.Scanner;
 
 public class AllocateArray {
