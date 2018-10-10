d18a5cb28fb6ab3a2fc81ae3fab4a4967eccc974
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:38:49 +0300

    Update ExchangeIfGreater.java

--- ExchangeIfGreater.java
+++ ExchangeIfGreater.java
@@ -1,3 +1,7 @@
+/*Write a program that reads two double values from the console A and B, stores them in variables and exchanges their values 
+if the first one is greater than the second one. Use an if-statement. As a result print the values of the 
+variables A and B, separated by a space.*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
