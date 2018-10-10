2ec93868a2475c12e6ad2ff8f4718022fbccdb02
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:43:33 +0300

    Update DigitAsWord.java

--- DigitAsWord.java
+++ DigitAsWord.java
@@ -1,3 +1,8 @@
+/*Write a program that read a digit [0-9] from the console, and depending on the input, shows the digit as a word (in English).
+
+Print "not a digit" in case of invalid input.
+Use a switch statement.*/
+
 import java.util.Scanner;
 
 public class DigitAsWord {
