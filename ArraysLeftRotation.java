3d4584781b99a0981ea5455ea549a4fb9f3bd6d6
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 10:02:46 +0300

    Update ArraysLeftRotation.java

--- ArraysLeftRotation.java
+++ ArraysLeftRotation.java
@@ -1,3 +1,7 @@
+/*Given an array "a" of "n" integers and a number "d" , 
+perform "d" left rotations on the array. Return the updated array to be printed 
+as a single line of space-separated integers.*/
+
 import java.util.Scanner;
 
 public class ArraysLeftRotation {
