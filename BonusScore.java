6f600a4687e3bec6c2fd69620b7c5e1d9f2907fd
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:39:20 +0300

    Update BonusScore.java

--- BonusScore.java
+++ BonusScore.java
@@ -1,3 +1,10 @@
+/*Write a program that applies bonus score to given score in the range [1…9] by the following rules:
+
+If the score is between 1 and 3, the program multiplies it by 10.
+If the score is between 4 and 6, the program multiplies it by 100.
+If the score is between 7 and 9, the program multiplies it by 1000.
+If the score is less than 0 or more than 9, the program prints "invalid score".*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
