bde4b31a14db24c0a2952b844f2e0dd56d27d7e9
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:35:20 +0300

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,3 +1,7 @@
+/*Write a program that, for a given two numbers N and x, calculates the sum S = 1 + 1!/x + 2!/x2 + … + N!/xN.
+
+Use only one loop. Print the result with 5 digits after the decimal point.*/
+
 import java.util.Scanner;
 
 public class Calculate {
