6e4e6fd6afbe7031cfbca59dc9affec2f353ca2a
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:40:06 +0300

    Update CheckForPlayCard.java

--- CheckForPlayCard.java
+++ CheckForPlayCard.java
@@ -1,3 +1,11 @@
+/*Classical play cards use the following signs to designate the card face: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K and A. 
+Write a program that enters a string and prints "yes CONTENT_OF_STRING" if it is a valid card sign or "no CONTENT_OF_STRING" otherwise.
+
+Input
+On the only line you will receive a single string.
+Output
+Output "yes CONTENT_OF_STRING" if the string is a card sign, otherwise print "no CONTENT_OF_STRING".*/
+
 import java.util.Scanner;
 
 public class CheckForPlayCard {
