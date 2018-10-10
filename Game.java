a51edbcddea2efa8a43d421528c062cb74b2c476
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:32:40 +0300

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,10 @@
+/*Three friends came up with a game for having fun in the break between the classes. One of them says a three-digit number and 
+the others use it to form a mathematical expressions by using operators for sum and multiplication between the digits of that number.
+
+The winner is the first one who founds the biggest number that is a result of the above mentioned rules.
+
+Write a program 'game', which prints out that biggest number.*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
