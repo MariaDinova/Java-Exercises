f49930f77b090e7855e1568288eb3269ce819e88
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:44:12 +0300

    Update IntDoubleString.java

--- IntDoubleString.java
+++ IntDoubleString.java
@@ -1,3 +1,9 @@
+/*Write a program that, depending on the first line of the input, reads an int, double or string variable.
+
+If the variable is int or double, the program increases it by one.
+If the variable is a string, the program appends * at the end.
+Print the result at the console. Use switch statement.*/
+
 import java.util.Scanner;
 
 public class IntDoubleString {
