cbef567d95c931a611899633b2b730729eaef87e
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:33:25 +0300

    Update NotDivisableNumber.java

--- NotDivisableNumber.java
+++ NotDivisableNumber.java
@@ -1,3 +1,6 @@
+/*Write a program that reads from the console a positive integer N and prints all the numbers 
+from 1 to N not divisible by 3 or 7, on a single line, separated by a space.*/
+
 import java.util.Scanner;
 
 public class NotDivisableNumber {
