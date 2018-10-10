6e90e76ffc7c1f4ed967fc5f568a588b75175ab0
MariaDinova <40626068+MariaDinova@users.noreply.github.com>
2018-10-10 09:26:34 +0300

    Update OddOrEvenProduct.java

--- OddOrEvenProduct.java
+++ OddOrEvenProduct.java
@@ -1,3 +1,14 @@
+/*ou are given N integers (given in a single line, separated by a space).
+
+Write a program that checks whether the product of the odd elements is equal to the product of the even elements.
+Elements are counted from 1 to N, so the first element is odd, the second is even, etc.
+Input
+On the first line you will receive the number N
+On the second line you will receive N numbers separated by a whitespace
+Output
+If the two products are equal, output a string in the format "yes PRODUCT_VALUE", otherwise write on the console 
+"no ODD_PRODUCT_VALUE EVEN_PRODUCT_VALUE"*/
+
 import java.util.Scanner;
 
 public class OddOrEvenProduct {
